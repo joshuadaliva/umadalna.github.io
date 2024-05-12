174b7af4dfc9dadc742c6b53ebc05bd757141dde
joshua <daliva@gmail.com>
2024-05-12 09:08:34 +0800

    --amend

--- signup.js
+++ signup.js
@@ -1,21 +1,76 @@
 document.getElementById("signup").addEventListener("click", accessHTML);
 document.getElementById("signupDetails").addEventListener("click", putDetails);
 
-let email, username, password;
+let username, email, password;
 
+window.signupDetails = {
+    username: username,
+    email: email,
+    password: password
+};
 
 // Function to access HTML
 function accessHTML() {
     window.location.href = "signup.html";
 }
 
-function putDetails(){
-    if (document.getElementById('name').value.trim() == "" || document.getElementById('email').value.trim() == "" ||  document.getElementById('password').value.trim() == ""){
-            alert("no value maybe in name, username or password please try again")
-        }
-    username = document.getElementById('name').value;
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
 
+function putDetails(){
+    if (document.getElementById('name').value.trim() == ""){
+        Swal.fire({
+            title: 'Sign Up error!',
+            text: "There's no name provided, please try again",
+            icon: 'error',
+            confirmButtonText: 'Try again'
+          })
+    }
+    else if(document.getElementById('email').value.trim() == ""){
+        Swal.fire({
+            title: 'Sign Up error!',
+            text: "There's no email provided, please try again",
+            icon: 'error',
+            confirmButtonText: 'Try again'
+          })
+        }
+    else if(document.getElementById('password').value.trim() == ""){
+        Swal.fire({
+            title: 'Sign Up error!',
+            text: "There's no password provided, please try again",
+            icon: 'error',
+            confirmButtonText: 'Try Again'
+          })
+        }   
+    else{
+        username = document.getElementById('name').value;
+        email = document.getElementById('email').value;
+        password = document.getElementById('password').value;
+        let timerInterval;
+        Swal.fire({
+        title: "Sign up Success",
+        icon: 'success',
+        html: "Moving to login page in <b></b> Seconds.",
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 50);
+            
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+        }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+            window.location.href = "login.html"
+        }
+        });
+        
+    }
+    
 }   
 
